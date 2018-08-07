12cc0ff83d5193f7b564c9b4f4a457b72b7f8e7e
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-08-07 22:38:55 +0200

    feat: db connection

--- src/db.js
+++ src/db.js
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose';
 
-mongoose.Promise = global.Promise;
-const uri = 'mongodb://';
 
-export const connect = () => mongoose.connect(uri);
+mongoose.Promise = global.Promise;
+const uri = 'mongodb://localhost:27017/vue-todo';
+
+export const connect = () => {
+  mongoose.connect(uri, { useNewUrlParser: true })
+    .then(conn => {
+      console.log(`db connected on port #${conn.connections[0].port}`);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
