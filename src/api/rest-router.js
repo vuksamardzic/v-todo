4f78228abdb2ff29df5650496486fb2edc3b96da
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-08-07 22:39:12 +0200

    feat: todo rest router

--- src/api/rest-router.js
+++ src/api/rest-router.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { apiErrorHandler } from './modules/error-handler';
+import { todoRouter } from './resources/todo/todo.router';
+
 
 export const restRouter = express.Router({});
 
+restRouter.use('/todo', todoRouter);
 restRouter.use(apiErrorHandler);
